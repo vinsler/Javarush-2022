bd70dcaf1c24ee56c3fd7e2e2fc13ad90d39fee6
vinsler <vinsler@yandex.ru>
2022-07-26 23:56:01 +0300

    some easy

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1702/Triangle.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task17.task1702;
+
+public class Triangle extends Shape {
+    //напишите тут ваш код
+
+    @Override
+    public void printInfo() {
+        System.out.println("Треугольник");
+    }
+}
