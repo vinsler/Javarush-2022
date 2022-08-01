60ce7048bbdb2970f4ff22e4db2dc2a2e4501d5b
vinsler <vinsler@yandex.ru>
2022-08-01 21:45:15 +0300

    some

--- 1.JavaSyntax/src/com/javarush/task/pro/task17/task1716/Vehicle.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1716/Vehicle.java
@@ -5,7 +5,11 @@ package com.javarush.task.pro.task17.task1716;
 */
 
 public interface Vehicle {
-    void start();
+    default void start() {
+        System.out.println("Начинаю движение.");
+    }
     void move();
-    void stop();
+    default void stop() {
+        System.out.println("Останавливаюсь.");
+    }
 }
