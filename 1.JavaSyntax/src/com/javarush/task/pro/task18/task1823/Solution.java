0c2760a378bb597cefbfd6562391e61bf6962ecc
vinsler <vinsler@yandex.ru>
2022-08-09 15:17:42 +0300

    1823

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1823/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task18.task1823;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toSet;
+
+/* 
+Из потока данных во множество
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var stringStream = Stream.of("JavaRush", "CodeGym", "Amigo", "Elly", "Kim", "Risha");
+
+        getFilteredStrings(stringStream).forEach(System.out::println);
+    }
+
+    public static Set<String> getFilteredStrings(Stream<String> stringStream) {
+        //напишите тут ваш код
+        //stringStream.filter(s -> (s.length() > 5)).collect(Collectors.toSet());
+        return stringStream.filter(s -> (s.length() > 5)).collect(Collectors.toSet());
+    }
+}
