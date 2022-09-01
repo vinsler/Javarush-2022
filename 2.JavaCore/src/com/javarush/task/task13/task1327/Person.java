043501f93abcbbf3002450ac5afb19199431aa48
vinsler <vinsler@yandex.ru>
2022-09-01 22:22:14 +0300

    1327-1328

--- 2.JavaCore/src/com/javarush/task/task13/task1327/Person.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1327/Person.java
@@ -1,6 +1,6 @@
 package com.javarush.task.task13.task1327;
 
-public class Person {
+public class Person implements RepkaItem{
     private String name;
     private String namePadezh;
 
@@ -23,5 +23,9 @@ public class Person {
 
     public void setNamePadezh(String namePadezh) {
         this.namePadezh = namePadezh;
+    }
+
+    public void pull(Person person) {
+        System.out.println(this.name + " за " + person.namePadezh);
     }
 }
---
+++ 2.JavaCore/src/com/javarush/task/task13/task1328/AbstractRobot.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task13.task1328;
+
+public abstract class AbstractRobot implements Attackable, Defensable {
+
+    @Override
+    public BodyPart attack() {
+        return null;
+    }
+
+    @Override
+    public BodyPart defense() {
+        return null;
+    }
+}
