27e326522208cfe03779343fc04d90e7c118f4d6
vinsler <vinsler@yandex.ru>
2022-09-02 12:39:13 +0300

    st

--- 2.JavaCore/src/com/javarush/task/task13/task1328/AbstractRobot.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1328/AbstractRobot.java
@@ -1,14 +1,47 @@
 package com.javarush.task.task13.task1328;
 
 public abstract class AbstractRobot implements Attackable, Defensable {
+    private static int hitCount;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
 
     @Override
     public BodyPart attack() {
-        return null;
+        BodyPart attackedBodyPart = null;
+        hitCount = hitCount + 1;
+
+        if (hitCount == 1) {
+            attackedBodyPart = BodyPart.ARM;
+        } else if (hitCount == 2) {
+            attackedBodyPart = BodyPart.HEAD;
+        } else if (hitCount == 3) {
+            attackedBodyPart = BodyPart.LEG;
+        } else {
+            hitCount = 0;
+            attackedBodyPart = BodyPart.CHEST;
+        }
+        return attackedBodyPart;
     }
 
     @Override
     public BodyPart defense() {
-        return null;
+        BodyPart defendedBodyPart = null;
+        hitCount = hitCount + 2;
+
+        if (hitCount == 1) {
+            defendedBodyPart = BodyPart.HEAD;
+        } else if (hitCount == 2) {
+            defendedBodyPart = BodyPart.LEG;
+        } else if (hitCount == 3) {
+            defendedBodyPart = BodyPart.ARM;
+        } else {
+            hitCount = 0;
+            defendedBodyPart = BodyPart.CHEST;
+        }
+        return defendedBodyPart;
     }
 }
--- 2.JavaCore/src/com/javarush/task/task13/task1328/Robot.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1328/Robot.java
@@ -1,7 +1,7 @@
 package com.javarush.task.task13.task1328;
 
-public class Robot implements Attackable, Defensable {
-    private static int hitCount;
+public class Robot extends AbstractRobot {
+    //private static int hitCount;
     private String name;
 
     public Robot(String name) {
@@ -12,33 +12,13 @@ public class Robot implements Attackable, Defensable {
         return name;
     }
 
+    @Override
     public BodyPart attack() {
-        BodyPart attackedBodyPart = null;
-        hitCount = hitCount + 1;
-
-        if (hitCount == 1) {
-            attackedBodyPart = BodyPart.ARM;
-        } else if (hitCount == 2) {
-            attackedBodyPart = BodyPart.HEAD;
-        } else if (hitCount == 3) {
-            hitCount = 0;
-            attackedBodyPart = BodyPart.LEG;
-        }
-        return attackedBodyPart;
+        return super.attack();
     }
 
+    @Override
     public BodyPart defense() {
-        BodyPart defendedBodyPart = null;
-        hitCount = hitCount + 2;
-
-        if (hitCount == 1) {
-            defendedBodyPart = BodyPart.HEAD;
-        } else if (hitCount == 2) {
-            defendedBodyPart = BodyPart.LEG;
-        } else if (hitCount == 3) {
-            hitCount = 0;
-            defendedBodyPart = BodyPart.ARM;
-        }
-        return defendedBodyPart;
+        return super.defense();
     }
 }
