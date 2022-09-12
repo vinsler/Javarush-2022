7cf4bb2a37be87e798eca10b73aceab55ff5bcb4
vinsler <vinsler@yandex.ru>
2022-09-12 10:46:51 +0300

    1415-1417

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1417/Hryvnia.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task14.task1417;
+
+public class Hryvnia extends Money {
+
+    public Hryvnia(double amount) {
+        super(amount);
+    }
+
+    @Override
+    public double getAmount() {
+        return super.getAmount();
+    }
+
+    @Override
+    public String getCurrencyName() {
+        return "UAH";
+    }
+}
