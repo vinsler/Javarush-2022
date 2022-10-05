ec7b4936bbeb7294432800bd887a6b0ed19a037e
vinsler <vinsler@yandex.ru>
2022-10-05 21:31:03 +0300

    1601-1605

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1603/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task16.task1603;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Список и нити
+*/
+
+public class Solution {
+    public static volatile List<Thread> list = new ArrayList<Thread>(5);
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        for (int i = 0; i < 5; i++) {
+            list.add(new Thread(new SpecialThread()));
+        }
+    }
+
+    public static class SpecialThread implements Runnable {
+        public void run() {
+            System.out.println("it's a run method inside SpecialThread");
+        }
+    }
+}
